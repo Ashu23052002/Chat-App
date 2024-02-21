0badc7dfb4d3bd0025c1b90138d89ac71912dd0c
Ashu23052002 <ans23052002@gmail.com>
2024-02-22 01:23:45 +0530

    signup

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,22 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Helloooo World!");
-});
+// middlewares
+app.use(express.json()) // to parse the incoming request with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes); // app.use([path] , middlewareFunction)
+
+
+
+// app.get("/", (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send("Helloooo World!");
+// });
+
+
+
+
+
 
 app.listen(PORT, () => {
   connectToMongoDB();
