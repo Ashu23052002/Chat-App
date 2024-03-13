c5b88c2e167f73d24122e392355759d2ea067d81
Ashu23052002 <ans23052002@gmail.com>
2024-03-13 22:34:12 +0530

    auth

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -2,38 +2,26 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { useAuthContext } from "../context/AuthContext";
 
-const useSignup = () => {
+const useLogin = () => {
   const [loading, setLoading] = useState(false);
   const { setAuthUser } = useAuthContext();
 
-  const signup = async ({
-    fullName,
-    username,
-    password,
-    confirmPassword,
-    gender,
-  }) => {
+  const login = async (username, password) => {
     const success = handleInputErrors({
-      fullName,
       username,
       password,
-      confirmPassword,
-      gender,
     });
 
     if (!success) return;
 
     setLoading(true);
     try {
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          fullName,
           username,
           password,
-          confirmPassword,
-          gender,
         }),
       });
 
@@ -56,28 +44,14 @@ const useSignup = () => {
     }
   };
 
-  return { loading, signup };
+  return { loading, login };
 };
 
-export default useSignup;
+export default useLogin;
 
-function handleInputErrors({
-  fullName,
-  username,
-  password,
-  confirmPassword,
-  gender,
-}) {
-  if (!fullName || !username || !password || !confirmPassword || !gender) {
+function handleInputErrors({ username, password }) {
+  if (!username || !password) {
     toast.error("Please fill all the fields");
-    return false;
-  }
-  if (password !== confirmPassword) {
-    toast.error("Password do not match");
-    return false;
-  }
-  if (password.length < 6) {
-    toast.error("Password must be atleast 6 characters");
     return false;
   }
 
