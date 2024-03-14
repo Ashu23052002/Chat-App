1624a4f80fadb57e4b3745347a3cf17579c23f1d
Ashu23052002 <ans23052002@gmail.com>
2024-03-14 22:44:27 +0530

    msg

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,30 @@
+import { useEffect } from "react";
 import MessageInput from "./MessageInput.jsx";
 import Messages from "./Messages.jsx";
 import { TiMessages } from "react-icons/ti";
+import useConversation from "../../zustand/useConversation.js";
 
 const MessageContainer = () => {
-  const nochatselected = true;
+  // const nochatselected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    // cleanup function (unmounts)
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {nochatselected ? (
+      {!selectedConversation ? (
         <NoChatSelect />
       ) : (
         <>
           {/* Header */}
           <div className="bg-slate-500 px-4 py-2 mb-2">
-            <span className="label-text">To:</span>
-            <span className="text-gray-900 font-bold">John doe</span>
+            <span className="label-text">To: </span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
           </div>
 
           <Messages />
